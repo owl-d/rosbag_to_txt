4dd1b0dfa001d494191091c1c7c2bf33a05a0deb
owl-d <limdoyou000921@gmail.com>
2023-06-14 16:41:11 +0900

    up

--- src/rosbag_to_txt.py
+++ src/rosbag_to_txt.py
@@ -4,23 +4,27 @@ import rospy
 from nav_msgs.msg import Odometry
 
 def callback(data):
-    scenario = "ex18"
-    file_path = "/home/doyu/doyu_ws/Polaris/odometry/A-LOAM/A-LOAM_" + scenario + ".txt"
-    is_first=True
-    print("Odometry save as txt : "+scenario)
+
+    ##### argument ######
+    scenario = "exp04"
+    algorithm = "A-LOAM"
+    #####################
+
+    file_path = "/home/doyu/doyu_ws/Polaris/trajectories/" + algorithm + "/" + algorithm + "_" + scenario + ".txt"
+    print("Odometry save as txt : scenario " + scenario)
 
     file = open(file_path, 'a')
     odometry = str(data.header.stamp) + ' ' \
         + str(data.pose.pose.position.x) + ' ' + str(data.pose.pose.position.y) + ' ' + str(data.pose.pose.position.z) + ' ' \
         + str(data.pose.pose.orientation.x) + ' ' + str(data.pose.pose.orientation.y) + ' ' \
         + str(data.pose.pose.orientation.z) + ' ' + str(data.pose.pose.orientation.w)
-    if is_first :
-        file.write(odometry)
-        is_first=False
-    else : file.write("\n"+odometry)
+    file.write("\n"+odometry)
     file.close()
 
 if __name__ == "__main__":
+
+    odom_topic = "/aft_mapped_to_init"
+
     rospy.init_node("estimated_odometry")
-    rospy.Subscriber("/aft_mapped_to_init", Odometry, callback)
+    rospy.Subscriber(odom_topic, Odometry, callback)
     rospy.spin()
